d26e3d01b2f583b2e3c1b002748f03e0a469dee3
Andrew <tkachenkoandrusha@gmail.com>
2025-02-18 22:26:44 +0200

    global

--- my-portfolio-app/src/Pages/Home.jsx
+++ my-portfolio-app/src/Pages/Home.jsx
@@ -107,6 +107,11 @@ function Home() {
           </div>
         ))}
       </div>
+      <section className='vpn-section'>
+        <h2>Huge Global Network of Fast VPN</h2>
+        <p>See LaslesVPN everywhere to make it easier for you when you move locations.</p>
+        <img src="/global.png" alt="glob" className="globalimage" />
+      </section>
     </main>
   );
 }
