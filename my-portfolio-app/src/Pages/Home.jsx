02192279e56198f2d9f59f300f180dbbab98f900
Andrew <tkachenkoandrusha@gmail.com>
2025-02-21 22:00:28 +0200

    customer

--- my-portfolio-app/src/Pages/Home.jsx
+++ my-portfolio-app/src/Pages/Home.jsx
@@ -119,6 +119,10 @@ function Home() {
         <img src="/discord.png" alt="Discord" class="logobr" />
         <img src="/spotify.png" alt="Spotify" class="logobr" />
       </div>
+      <div className='customer'>
+        <h2>Trusted by Thousands of Happy Customer</h2>
+        <p>These are the stories of our customers who have joined us with great pleasure when using this crazy feature.</p>
+      </div>
     </main>
   );
 }
